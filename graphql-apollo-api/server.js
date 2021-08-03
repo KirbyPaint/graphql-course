94c4bc12a1cb60a57d599d401b86f3420fd8ebf0
Ashley Porter <porter.ashley13@gmail.com>
2021-08-03 16:17:59 -0700

    add lesson 4.19 - Create Task Mutation

--- graphql-apollo-api/server.js
+++ graphql-apollo-api/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { ApolloServer, gql } = require('apollo-server-express');
 const cors = require('cors');
 const dotEnv = require('dotenv');
+const uuid = require('uuid');
 
 const { tasks, users } = require('./constants')
 
@@ -23,6 +24,16 @@ const typeDefs = gql`
     task(id: ID!): Task
     users: [User!]
     user(id: ID!): User
+  }
+
+  input createTaskInput {
+    name: String!
+    completed: Boolean!
+    userId: ID!
+  }
+
+  type Mutation {
+    createTask(input: createTaskInput!): Task
   }
 
   type User {
@@ -54,6 +65,13 @@ const resolvers = {
     user: (_, { id }) => {
       return users.find(user => user.id === id)
     },
+  },
+  Mutation: {
+    createTask: (_, { input }) => {
+      const task = { ...input, id: uuid.v4() }
+      tasks.push(task);
+      return task;
+    }
   },
   Task: {
     user: ({ userId }) => {
