ddca82be180e02de2f1b3252b8d51ed0deba428c
Ashley Porter <porter.ashley13@gmail.com>
2021-08-03 17:36:13 -0700

    add lesson 4.24 - Signup Mutation

--- graphql-apollo-api/resolvers/user.js
+++ graphql-apollo-api/resolvers/user.js
@@ -1,4 +1,7 @@
+const bcrypt = require('bcryptjs');
+
 const { users, tasks } = require('../constants');
+const User = require('../database/models/user');
 
 module.exports = {
   Query: {
@@ -8,6 +11,21 @@ module.exports = {
     },
   },
   Mutation: {
+    signup: async (_, { input }) => {
+      try {
+        const user = await User.findOne({ email: input.email });
+        if (user) {
+          throw new Error('Email already in use');
+        }
+        const hashedPassword = await bcrypt.hash(input.password, 12);
+        const newUser = new User({ ...input, password: hashedPassword });
+        const result = await newUser.save();
+        return result;
+      } catch (error) {
+        console.log('error: ', error);
+        throw error;
+      }
+    }
   },
   User: {
     tasks: ({ id }) => tasks.filter(task => task.userId === id)
--- graphql-apollo-api/typeDefs/user.js
+++ graphql-apollo-api/typeDefs/user.js
@@ -1,15 +1,27 @@
 const { gql } = require('apollo-server-express');
 
 module.exports = gql`
-extend type Query {
-  users: [User!]
-  user(id: ID!): User
-}
+  extend type Query {
+    users: [User!]
+    user(id: ID!): User
+  }
 
-type User {
-  id: ID!
-  name: String!
-  email: String!
-  tasks: [Task!]
-}
+  extend type Mutation {
+    signup(input: signupInput): User
+  }
+
+  input signupInput {
+    name: String!
+    email: String!
+    password: String!
+  }
+
+  type User {
+    id: ID!
+    name: String!
+    email: String!
+    tasks: [Task!]
+    createdAt: String!
+    updatedAt: String!
+  }
 `;
