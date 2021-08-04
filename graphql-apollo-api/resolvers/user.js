32741ead597004ef2ae6a76febbf443fea36e1e6
Ashley Porter <porter.ashley13@gmail.com>
2021-08-04 13:27:36 -0700

    add lesson 4.30 - UPDATING: Get User By Id

--- graphql-apollo-api/resolvers/user.js
+++ graphql-apollo-api/resolvers/user.js
@@ -2,16 +2,23 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { combineResolvers } = require('graphql-resolvers');
 
-const { users, tasks } = require('../constants');
 const User = require('../database/models/user');
+const Task = require('../database/models/task');
 const { isAuthenticated } = require('./middleware');
 
 module.exports = {
   Query: {
-    users: () => users,
-    user: combineResolvers(isAuthenticated, (_, { id }, { email }) => {
-      console.log('email: ', email);
-      return users.find(user => user.id === id)
+    user: combineResolvers(isAuthenticated, async (_, __, { email }) => {
+      try {
+        const user = await User.findOne({ email });
+        if (!user) {
+          throw new Error('User not found!');
+        }
+        return user
+      } catch (error) {
+        console.log('error: ', error);
+        throw error;
+      }
     }),
   },
   Mutation: {
@@ -50,6 +57,14 @@ module.exports = {
     },
   },
   User: {
-    tasks: ({ id }) => tasks.filter(task => task.userId === id)
+    tasks: async ({ id }) => {
+      try {
+        const tasks = await Task.find({ user: id })
+        return tasks;
+      } catch (error) {
+        console.log('error: ', error);
+        throw error;
+      }
+    }
   }
 }
